250e02abcd19325140e5fc562d81f39219bc49b0
paul-leonard <paul.ryan.leonard@gmail.com>
2020-07-26 11:57:40 -0700

    added placeholder content to all subpages including an input form on contact page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function reactToCustomerContactSubmit(event) {
+  event.preventDefault();
+  console.log('function being called');
+
+  var incomingUserName = event.target.userName.value;
+  var incomingUserEmail = event.target.userEmail.value;
+  var incomingUserPhone = event.target.userPhone.value;
+
+  alert('Hello, ' + incomingUserName + '!  Thanks for visiting our site and your interest in our services!  Sadly, this form currently goes nowhere.  Please give us a call or shoot us an email instead.  Thanks and hope to talk to you soon!');
+
+}
+
+var customerSubmitButtonEL = document.getElementById('customercontact');
+customerSubmitButtonEL.addEventListener('submit',reactToCustomerContactSubmit);
+
+console.log('JS ran');
